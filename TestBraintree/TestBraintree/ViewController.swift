44d65a856974d4045daa41a8e5482334717cdd8e
Michael Busheikin <mike@buttonmashgames.com>
2015-09-09 20:41:02 -0400

    First commit. Steps followed from https://github.com/braintree/braintree_ios/blob/master/Docs/Manual%20Integration.md  Compilation fails.

---
+++ TestBraintree/TestBraintree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestBraintree
+//
+//  Created by Michael Busheikin on 2015-09-09.
+//  Copyright (c) 2015 aftrapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
